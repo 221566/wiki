9c6b161fce0a70465cfaf75cc6c0d72cef1eab98
221566 <2215668921@qq.com>
2021-09-19 00:09:08 +0800

    封装请求参数和返回参数

--- src/main/java/com/lwx/wiki/controller/EbookController.java
+++ src/main/java/com/lwx/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.lwx.wiki.controller;
 
-import com.lwx.wiki.domain.Ebook;
+import com.lwx.wiki.req.EbookReq;
 import com.lwx.wiki.resp.CommonResp;
+import com.lwx.wiki.resp.EbookResp;
 import com.lwx.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @RequestMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
